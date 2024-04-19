2d789de36c35466f3c8c65dcb6f74eb9a0c31342
t480s <abcd0425@gmail.com>
2024-04-19 16:21:03 -0700

    module 1 fix is done. now it has the duplicate feature.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import getcode
 
-secret_code = getcode.get_code(total_values=8, duplicate=False)
+secret_code = getcode.get_code(total_values=4, duplicate=False)
 
 print("secret_code:", secret_code)
