ddac0a391467633cc7d9d80b05753b549711e00f
t480s <abcd0425@gmail.com>
2024-04-19 16:52:48 -0700

    create validate.py

---
+++ validate.py
@@ -0,0 +1,4 @@
+# this function is to validate if the user's attempt is correct or not
+# the code will specify if correct number and if correct position (for full visibility)
+# based on the difficulty level, the code would choose what to reveal or not
+# this code would provide the full visibility of the info (number and position)
