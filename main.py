303c06b5a05930b6646c9e23badcdab21372ede8
t480s <abcd0425@gmail.com>
2024-04-20 13:29:09 -0700

    finish the validation on the user input attempt

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ import show_result
 
 print("Hello, are you ready for the game?")
 
-# the follow is to be done.
-# input the difficulty level: total_values, duplicates...
 
 ### user input
 difficulty_level = 1
@@ -14,14 +12,10 @@ total_values = 8
 ###
 
 
-
-
 #####################
-# Initialize the number of attempts and maximum attempts
 num_attempts = 1
 max_attempts = 10
 
-# Define the secret code
 secret_code = getcode.get_code(total_values, duplicate)
 
 # Future task: this would take a wile to generate non duplicate secret code
@@ -31,19 +25,30 @@ print("Secret code is generated!", secret_code)
 # Initialize an empty list to store user inputs
 user_attempts = []
 
-# Start the while loop
-while num_attempts < max_attempts + 1:
-    # Ask the user to input a sequence of four numbers
-    user_input = input("Guess a sequence of four numbers (example: 3102): ")
+def validate_input(user_input):
+    if len(user_input) != 4:
+        return False
+    if not user_input.isdigit():
+        return False
+    return True
 
-    # Convert the input string into a list of integers
+# while loop for 10 attempts
+while num_attempts < max_attempts + 1:
+    is_user_input_valid = False
+
+    while is_user_input_valid is False:
+        user_input = input("Guess a sequence of four numbers (example: 3102): ")
+        if validate_input(user_input):
+            is_user_input_valid = True
+        else:
+            print("Please input 4 digit of numbers.")
+
     user_attempt = [int(digit) for digit in user_input]
     print(f"Your Guess Attempt {num_attempts}:", user_attempt)
+        
 
-    # Increment the number of attempts
     num_attempts += 1
 
-    # Append the user attempt to the list of attempts
     user_attempts.append(user_attempt)
     
     number_boolean, position_boolean, counter_correct_number = \
@@ -54,11 +59,9 @@ while num_attempts < max_attempts + 1:
     
     print('--------------------------')
 
-    # Check if the user's attempt matches the secret code
     if user_attempt == secret_code:
         print("Congratulations! You guessed the code correctly!")
-        break  # Exit the loop if the code is guessed correctly
-
-# Check if the user has exhausted all attempts
+        break
+    
 if num_attempts == max_attempts + 1:
     print("Sorry, you've used all your attempts. The secret code is:", secret_code)
